e755dc594333bc198aa2df33625c9a41398d2ed3
nadeem <abdulnadeemnihaal@gmail.com>
2024-01-13 22:42:54 +0530

    i change the program but it have a error

--- script.js
+++ script.js
@@ -1,77 +1,79 @@
 
-let boxes = document.querySelectorAll(".box");
+const cells=document.querySelectorAll(".cell");
+const statusText=document.querySelector("#statusText");
+const restart=document.querySelector("#restartBtn");
+const winConditions = [
+    [ 0, 1, 2] , 
+    [ 3, 4, 5] ,
+    [ 6, 7, 8] ,
+    [ 0, 3, 6] ,
+    [ 1, 4, 7] , 
+    [ 2, 5, 8] ,
+    [ 0, 4, 8] , 
+    [ 2, 4, 6] 
 
-let turn ="X";
+];
+let options =["", "", "", "", "", "", "", "", ""]
+let currentPlayer ="X";
+let running =false;
 
-let isGameOver =false;
+initializeGame();
 
-boxes.forEach(e =>{
-         e.innerHTML="";
-        e.addEventListener("click", ()=>{
-            if(! isGameOver && e.innerHTML ===""){
-                e.innerHTML= turn;
-                cheakWin();
-                cheakDraw();
-                changeTurn();
-                  
-            }
-        })
-})
-
-function changeTurn(){
-    if(turn ==="X"){
-     turn ="O";
-    document.querySelector(".bg").style.left ="85px";
-}else{
-    turn ="X";
-    document.querySelector(".bg").style.left ="0";
-  }
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click",cellClicked))
+    restartBtn.addEventListener("click",restartGame);
+    statusText.textContent =`${currentPlayer}'s turn`;
+    running =true;
 }
-function cheakWin(){
-let WinCondtions = [
-         [ 0, 1, 2] , [ 3, 4, 5] , [ 6, 7, 8] ,
-         [ 0, 3, 6] , [ 1, 4, 7] , [ 2, 5, 8] ,
-         [ 0, 4, 8] , [ 2, 4, 6] 
-]
-for(let i=0; i<WinCondtions.length; i++){
-    let v0=boxes[WinCondtions[i][0].innerHTML];
-    let v1=boxes[WinCondtions[i][1].innerHTML];
-    let v2=boxes[WinCondtions[i][2].innerHTML];
-    
-    if(v0 !="" && v0 === v1 && v0 === v2){
-     isGameOver= true;
-     document.querySelector("#results").innerHTML = turn + "win";
-     document.querySelector("#play-again").style.display ="inline"; 
-    for(j =0;j<3;j++){
-        boxes[WinCondtions[i][j]].style.backgroundcolor ="#08D9D6";
-        boxes[WinCondtions[i][j]].style.color = "#000";
+function cellClicked(){
+    const cellIndex =this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "  || !running"){
+        return;
+    }
+    updateCell(this,cellIndex);
+    checkWinner();
+}
+function updateCell(cell,index){
+   options[index] =currentPlayer;
+   cell.textContent =currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer =="X") ? "O" : "X";
+    statusText.textContent = `$(currentplayer)'s turn`;
+}
+function checkWinner(){
+    let roundWon =false;
+    for(let i= 0;i <winConditions.length;i++){
+        const condition =winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA ==""   || cellB == "" ||  cellC == ""){
+            continue;
         }
-      }
+        if(cellA ==cellB &&cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+        running = false;
+    }
+    else if (!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running =false;
+    }
+    else{
+changePlayer();
     }
 }
-function cheakDraw(){
-     if(!isGameOver){
-         let isDraw = true;
-        boxes.forEach(e =>{
-            if(e.innerHTML ==="")isDraw= false;
-        })
-        if(isDraw){
-            isGameOver= true;
-            document.querySelector("#results").innerHTML = "Draw";
-            document.querySelector("#play-again").style.display ="inline"; 
-        }
-     }
+function restartGame(){
+   currentPlayer = "X";
+   options =["", "", "", "", "", "", "", "", ""];
+   statusText.textContent =`${currentPlayer}'s turn`;
+   cells.forEach(cell => cell.textContent = "");
+   running =true;
 }
-document.querySelector("#playagain").addEventListener("click",()=> {
-    isGameOver =false;
-    turn="X";
-    document.querySelector(".bg").style.left ="0";
-    document.querySelector("#results").innerHTML="";
-    document.querySelector("#play-again").style.display ="none";
-
-    boxes.forEach(e =>{
-        e.innerHTML="";
-        e.style.removeProperty("background-color");
-        e.style.color = "#fff";
-    })
-})
